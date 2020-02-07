1ef71434531e6a9e9ec032f31ec3e4bc0d3f1171
amor <cn.coder@tom.com>
2020-02-07 18:49:36 +0800

    a

--- bin/build.js
+++ bin/build.js
@@ -6,7 +6,8 @@ const compiler = require('./compiler')
 
 let rootPath = `${process.cwd()}/${process.argv[2]}`
 
-function buildPage(path, name = 'index') {
+function buildPage(path, name, callback) {
+	console.log('开始编译' + name + '模板')
 	let result = compiler({
 		html: fs.readFileSync(`${path}/index.html`, 'utf-8'),
 		css: fs.readFileSync(`${path}/index.css`, 'utf-8'),
@@ -15,19 +16,30 @@ function buildPage(path, name = 'index') {
 		name: name
 	}).result
 	
-	fse.emptyDirSync('dist')
 	fse.outputFileSync(`dist/${result.moduleName}/index.html`, result.html,'utf8')
 	fse.outputFileSync(`dist/${result.moduleName}/${result.otherName}.css`, result.css, 'utf8')
 	fse.outputFileSync(`dist/${result.moduleName}/${result.otherName}.js`, result.js, 'utf8')
 	console.log('成功')
+	callback();
+}
+
+function buildTask(modules) {
+	if(modules) {
+		if(modules.length) {
+			let item = modules.shift()
+			if(fse.statSync(`${rootPath}/${item}`).isDirectory()) {
+				console.log('开始Build' + item)
+				buildPage(`${rootPath}/${item}`, item, function() {
+					buildTask(modules)
+				})
+			}
+		}
+	}
 }
 
 fse.readdir(rootPath, (err, files) => {
 	console.log(err)
 	console.log(files)
-	files.forEach((item) => {
-		if(fse.statSync(`${rootPath}/${item}`).isDirectory()) {
-			buildPage(`${rootPath}/${item}`, item)
-		}
-	}) 
+	fse.emptyDirSync('dist')
+	buildTask(files)
 })
